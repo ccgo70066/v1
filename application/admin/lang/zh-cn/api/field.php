ad334f6742606e9ae1f673ea6bef9bb579632eed
majkey <majkey@126.com>
2026-03-20 15:48:29 +0800

    feat: api filed

---
+++ application/admin/lang/zh-cn/api/field.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Id'          => 'ID',
+    'Name'        => '名称',
+    'Memo'        => '备注',
+    'Create_time' => '创建时间',
+    'Update_time' => '更新时间'
+];
---
+++ public/assets/js/backend/api/field.js
@@ -0,0 +1,65 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'api/field/index' + location.search,
+                    add_url: 'api/field/add',
+                    edit_url: 'api/field/edit',
+                    del_url: 'api/field/del',
+                    multi_url: 'api/field/multi',
+                    import_url: 'api/field/import',
+                    table: 'api_field',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'name', title: __('Name'), operate: 'LIKE'},
+                        {field: 'memo', title: __('Memo'), operate: 'LIKE'},
+                        {field: 'create_time', title: __('Create_time'), operate:'RANGE', addclass:'datetimerange', autocomplete:false},
+                        // {field: 'update_time', title: __('Update_time'), operate:'RANGE', addclass:'datetimerange', autocomplete:false},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            var selectRow = parent.$('#table').bootstrapTable('getSelections');
+            if (selectRow.length > 0) {
+                selectRow = selectRow[0]
+                var rowArr = $("form").serializeArray();
+                rowArr.forEach(function (item, index) {
+                    var key = item.name.slice(item.name.indexOf("[") + 1, item.name.indexOf("]"));
+                    if (!['delete_time'].includes(key)) {
+                        $('[name="' + item.name + '"]').val(selectRow[key]);
+                    }
+                })
+            }
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
