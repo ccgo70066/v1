6fb25f3016deaaee7ba35af86f56ca4514f722fe
majkey <majkey@126.com>
2026-03-24 11:53:23 +0800

    remove addon

--- application/extra/addons.php
+++ application/extra/addons.php
@@ -9,16 +9,6 @@ return [
         'module_init' => [
             'apilog',
         ],
-        'view_filter' => [
-            'betterform',
-        ],
-        'config_init' => [
-            'betterform',
-            'summernote',
-        ],
-        'admin_login_init' => [
-            'loginbg',
-        ],
         'sms_send' => [
             'smsbao',
         ],
@@ -27,9 +17,6 @@ return [
         ],
         'sms_check' => [
             'smsbao',
-        ],
-        'app_init' => [
-            'socket',
         ],
     ],
     'route' => [],
