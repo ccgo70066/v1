faa0c80141f0fe08739e42b7fec294142e5bd211
majkey <majkey@126.com>
2026-04-10 19:50:11 +0800

    git

--- public/git.php
+++ public/git.php
@@ -30,4 +30,5 @@ if ($content['commits'][0]['author']['name'] == 'handsomeTaoTao') {
 
 echo shell_exec("cd {$local} && sudo  git pull");
 var_dump('test');
+var_dump('t1');
 die("done " . date('Y-m-d H:i:s', time()));
