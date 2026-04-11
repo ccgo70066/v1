a98dcea7accf6958d277841b9c1f7e990f015b9f
majkey <majkey@126.com>
2026-04-12 00:51:29 +0800

    backend room_profit

---
+++ application/admin/lang/zh-cn/room/profit.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Id'              => 'ID',
+    'Room_id'         => '房间',
+    'Val'             => '礼物流水值',
+    'Reward_val'      => '流水奖励值',
+    'Used_reward_val' => '已兑换流水奖励值',
+    'Create_time'     => '创建时间'
+];
---
+++ public/assets/js/backend/room/profit.js
@@ -0,0 +1,66 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'room/profit/index' + location.search,
+                    add_url: 'room/profit/add',
+                    edit_url: 'room/profit/edit',
+                    del_url: 'room/profit/del',
+                    multi_url: 'room/profit/multi',
+                    import_url: 'room/profit/import',
+                    table: 'room_profit',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'room_id', title: __('Room_id')},
+                        {field: 'val', title: __('Val'), operate:'BETWEEN'},
+                        {field: 'reward_val', title: __('Reward_val'), operate:'BETWEEN'},
+                        {field: 'used_reward_val', title: __('Used_reward_val'), operate:'BETWEEN'},
+                        {field: 'create_time', title: __('Create_time'), operate:'RANGE', addclass:'datetimerange', autocomplete:false},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            var selectRow = parent.$('#table').bootstrapTable('getSelections');
+            if (selectRow.length > 0) {
+                selectRow = selectRow[0]
+                var rowArr = $("form").serializeArray();
+                rowArr.forEach(function (item, index) {
+                    var key = item.name.slice(item.name.indexOf("[") + 1, item.name.indexOf("]"));
+                    if (!['delete_time'].includes(key)) {
+                        $('[name="' + item.name + '"]').val(selectRow[key]);
+                    }
+                })
+            }
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
