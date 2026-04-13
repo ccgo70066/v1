416cc93fdce25f5e98711dc5b0fa6d09d994df53
majkey <majkey@126.com>
2026-04-13 17:56:20 +0800

    room

--- application/api/controller/Member.php
+++ application/api/controller/Member.php
@@ -38,7 +38,7 @@ class Member extends Base
         $role = db('room_admin')->where('user_id', $user_id)->where('room_id', $room_id)->where('status', '>', 0)->find();
         $room['role'] = $role['role'] ?? 0;
         $room['role_status'] = $role['status'] ?? 0;
-        $room['hot'] = $redis->hGet(RedisService::ROOM_HOT_KEY, $room_id) ?: null;
+        $room['hot'] = $redis->hGet(RedisService::ROOM_HOT_KEY, $room_id) ?: 0;
         $profit = db('room_profit')->where('room_id', $room_id)->find();
         $room['gift_value'] = $profit['gift_val'] ?? 0;
         $room['reward_value'] = $profit['reward_val'] ?? 0;
