ca1fe0c6d1b64622a5a8bfa4ea075eb41a1d0b86
majkey <majkey@126.com>
2026-04-16 19:53:57 +0800

    user

--- application/api/controller/UserInfo.php
+++ application/api/controller/UserInfo.php
@@ -9,6 +9,7 @@ use app\common\service\MallService;
 use app\common\service\UserService;
 use think\Db;
 use think\Log;
+use util\Minio;
 
 
 /**
@@ -105,6 +106,85 @@ class UserInfo extends Base
         Db::name('user_audit_image')->where('id', input('id'))->delete();
         $this->success('ok');
     }
+
+
+    /**
+     * @ApiTitle    (添加需要审核的形象照)
+     * @ApiMethod   (post)
+     * @ApiParams   (name="url", type="string", required=true, rule="", description="图片URL")
+     */
+    public function add_image()
+    {
+        $user_id = $this->auth->id;
+        $imageStr = input('url');
+
+        $where = ['user_id' => $user_id, 'img_type' => 'image'];
+        $images1 = db('user_audit_image')->where($where)->where('status', 1)->count();
+        $images0 = db('user_audit_image')->where($where)->where('status', 0)->count();
+        if ($images1 < 9 && ($images1 + $images0) >= 9) {
+            $this->error(__('You can only upload a maximum of %s files', 9));
+        }
+
+        Db::startTrans();
+        try {
+            $deleteImages = [];
+            $where = ['user_id' => $user_id, 'img_type' => 'image', 'status' => 1];
+            if (get_site_config('user_image_audit') == 1) {
+                if ($images1 >= 9) {
+                    //删除待审核图片
+                    $deleteImages = db('user_audit_image')->where($where)->order('update_time asc')->limit(1)->column('url');
+                    db('user_audit_image')->where($where)->order('update_time asc')->limit(1)->delete();
+                }
+                $insertData[] = [
+                    'url'         => $imageStr,
+                    'status'      => 0,
+                    'user_id'     => $user_id,
+                    'img_type'    => 'image',
+                    'create_time' => datetime()
+                ];
+                db('user_audit_image')->insertAll($insertData);
+            } else {
+                if ($images1 >= 9) {
+                    //删除待审核图片
+                    $deleteImages = db('user_audit_image')->where($where)->order('update_time asc')->limit(1)->column('url');
+                    db('user_audit_image')->where($where)->order('update_time asc')->limit(1)->delete();
+                }
+
+                $insertData[] = [
+                    'url'         => $imageStr,
+                    'status'      => 1,
+                    'user_id'     => $user_id,
+                    'img_type'    => 'image',
+                    'create_time' => datetime()
+                ];
+                db('user_audit_image')->insertAll($insertData);
+
+                $images = db('user_audit_image')->where('user_id', $user_id)->where('status', 1)->column('url');
+                if (count($images)) {
+                    $imagesStr = implode(',', $images);
+                    $updateImage = trim($imagesStr, ',');
+                } else {
+                    $updateImage = $imageStr;
+                }
+                db('user')->where('id', $user_id)->update(['image' => $updateImage, 'create_time' => datetime()]);
+            }
+
+            if ($deleteImages) {
+                $minio = new Minio();
+                foreach ($deleteImages as $deleteImage) {
+                    $minio->deleteObject($deleteImage);
+                }
+            }
+
+            Db::commit();
+        } catch (\Exception $e) {
+            Db::rollback();
+            Log::error($e->getMessage());
+            $this->error($e->getMessage());
+        }
+        $this->success();
+    }
+
 
     /**
      * @ApiTitle (更新形象照)
