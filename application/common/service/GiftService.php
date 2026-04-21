12203fc83cebdd8f914184bd582803632abeccf7
majkey <majkey@126.com>
2026-04-21 15:05:56 +0800

    room

--- application/common/service/GiftService.php
+++ application/common/service/GiftService.php
@@ -83,7 +83,7 @@ class GiftService extends BaseService
                     GiftService::instance()->wall_add($to_user_id, $gift['gift_id'], $gift['count']);
                 }
                 user_business_change($to_user_id, 'reward_amount', $total_price[$to_user_id] * $user_rate, 'increase', substr($note, 0, -1), 4);
-                room_profit_statistics($room_id, $total_price[$to_user_id], $room_rate, $to_user_id);
+                room_profit_statistics($room_id, $total_price[$to_user_id], $room_rate * $total_price[$to_user_id], $to_user_id);
                 RankService::instance()->count_up($user_id, $to_user_id, $room_id, $total_price[$to_user_id]);
             }
             if ($source == 2) {
