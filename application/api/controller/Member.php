a08651b617fff81f681a5b70e099fb8ffd05160d
majkey <majkey@126.com>
2026-04-21 15:27:24 +0800

    room

--- application/api/controller/Member.php
+++ application/api/controller/Member.php
@@ -64,7 +64,7 @@ class Member extends Base
         $status = input('status', 1);
         $where = $status == 1 ? ['a.status' => ['in', '1,2']] : ['a.status' => $status];
         $list = db('room_admin')->alias('a')->join('user u', 'a.user_id = u.id', 'left')
-            ->field('a.user_id,a.status,u.id,u.nickname,u.avatar,u.birthday,gender,level,a.create_time,a.update_time' . $extend)
+            ->field('a.user_id,a.role,a.status,u.id,u.nickname,u.avatar,u.birthday,gender,level,a.create_time,a.update_time' . $extend)
             ->where('a.room_id', $room_id)->where($where)
             ->page(input('page', 1), input('size', 10))->select();
         $user_flow = db('user_follow')->where('user_id', $user_id)->whereIn('to_user_id', array_column((array)$list, 'id'))->column('id', 'to_user_id');
