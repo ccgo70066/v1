ad2efd24ac4262e0a3b215399b351cdc4598b5ac
majkeyabc <majkey@126.com>
2026-04-27 12:07:25 +0800

    fix

--- application/route.php
+++ application/route.php
@@ -4,8 +4,4 @@ use think\Route;
 
 $rs = db('api_route_confuse')->cache(true, 3600, 'api_route_confuse')->column('concat("/api", name)', 'encrypt');
 
-Route::any('/api/:path*', function () {
-    return '禁止访问API模块';
-})->method('*')->completeMatch(false);
-
 return $rs;
