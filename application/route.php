1b42cce2705434e0a312b369ffe27d864d716eb0
majkeyabc <majkey@126.com>
2026-04-27 12:32:30 +0800

    route

--- application/route.php
+++ application/route.php
@@ -3,5 +3,8 @@
 use think\Route;
 
 $rs = db('api_route_confuse')->cache(true, 3600, 'api_route_confuse')->column('concat("api", name)', 'encrypt');
+Route::rule('api/:_miss_', function () {
+    return response('', 404);
+}, '*', [], ['_miss_' => '[\\s\\S]+']);
 
-return $rs;
+return $rs ?: [];
