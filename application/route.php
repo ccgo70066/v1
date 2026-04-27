4d4c01d13e6a0db21d8da4b0a4970dd7f32a6ea7
majkeyabc <majkey@126.com>
2026-04-27 14:44:48 +0800

    route

--- application/route.php
+++ application/route.php
@@ -2,18 +2,19 @@
 
 use think\Route;
 
-$rs = db('api_route_confuse')
+$dbRoutes = db('api_route_confuse')
     ->cache(true, 3600, 'api_route_confuse')
     ->column('concat("api", name)', 'encrypt');
 
-$manualRoutes = [
-    'api/test' => 'api/test/index',
-];
+if (!empty($dbRoutes)) {
+    foreach ($dbRoutes as $route => $target) {
+        Route::rule($route, $target);
+    }
+}
 
-$rs = array_merge($rs ?: [], $manualRoutes);
-
+Route::any('api/test$', 'api/test/index');
+Route::any('api/test/:action', 'api/test/:action');
 Route::rule('api/:_miss_', function () {
     return response('', 404);
 }, '*', [], ['_miss_' => '[\\s\\S]+']);
 
-return $rs;
